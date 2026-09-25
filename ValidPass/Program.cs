74b6fde4170ba7846cad5e2050b5948047a1e39f
Judith Buck <jbuck@heckgrammar.co.uk>
2026-09-25 14:57:47 +0100

    ValidPass project added

---
+++ ValidPass/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ValidPass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            bool isValid = ValidPassword(password);
+
+            Console.WriteLine(isValid);
+        }
+        static bool ValidPassword(string pass)
+        {
+            return pass.Length >= 8 &&
+                   pass.Length <= 15 &&
+                   pass.Any(char.IsDigit) &&
+                   pass.Any(char.IsLetter) &&
+                   (pass.Any(char.IsSymbol) || pass.Any(char.IsPunctuation));
+        }
+    }
+}
